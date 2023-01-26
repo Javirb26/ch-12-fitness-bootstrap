d0dd831719e5586a7a173918faed62a440cdf846
Javirb26 <112392832+Javirb26@users.noreply.github.com>
2023-01-26 11:15:25 -0500

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Javier Ballester
+    File Name: script.js
+    Date: January 4, 2023
+*/
+
+//jQuery for hero image to consume the header window space
+$(document) .ready(function(){
+    $('.hero').height($(window).height());
+});
